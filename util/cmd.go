e9f9eb2c4c02efd0392de1eb826e99a6275653b1
zen <zhangyiming748@gmail.com>
2024-04-16 17:55:09 +0800

    判断古希腊掌管退出信号的文件是否存在

--- util/cmd.go
+++ util/cmd.go
@@ -49,9 +49,25 @@ func ExecCommand(c *exec.Cmd, msg string) (e error) {
 			slog.Debug("命令执行完毕", slog.String("输出", string(output)))
 		}
 	}
-	if exit := GetExitStatus(); exit {
-		slog.Debug("命令端获取到退出状态,命令结束后退出", slog.Bool("信号值", exit), slog.String("最后一条命令", fmt.Sprint(c)))
+	if isExitLabel() {
+		slog.Debug("命令端获取到退出状态,命令结束后退出", slog.String("最后一条命令", fmt.Sprint(c)))
 		os.Exit(0)
 	}
 	return nil
+}
+
+/*
+判断古希腊掌管退出信号的文件是否存在
+*/
+func isExitLabel() bool {
+	filePath := "/exit"
+
+	_, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		fmt.Println("古希腊掌管退出信号的文件不存在")
+		return false
+	} else {
+		fmt.Println("古希腊掌管退出信号的文件存在")
+		return true
+	}
 }
